24638e529a0e73fca2d916b273dfc78247ed7565
Wescley Martins <wescleymartins02@gmail.com>
2023-02-27 09:28:53 -0300

    Install and up api project on github actions

---
+++ tests/support/Pages/tasks/index.ts
@@ -0,0 +1,51 @@
+import { Page, expect, Locator } from '@playwright/test'
+import { TaskModel } from '../../../fixtures/task.model'
+
+export class TasksPage {
+    readonly page: Page
+    readonly inputTaskName: Locator
+
+    constructor(page: Page){
+        this.page = page
+        this.inputTaskName = page.locator('input[placeholder="Add a new Task"]')
+    }
+
+    async go(){
+        await this.page.goto('/')
+    }
+
+    async create(task: TaskModel) {
+        await this.inputTaskName.fill(task.name)
+        await this.page.click('css=button >> text=Create')
+    }
+
+    async toggle(taskName: string){
+        const target = this.page.locator(`xpath=//p[contains(text(), "${taskName}")]/..//button[contains(@class, "Toggle")]`)
+        await target.click()
+    }
+
+    async remove(taskName: string){
+        const target = this.page.locator(`xpath=//p[contains(text(), "${taskName}")]/..//button[contains(@class, "Delete")]`)
+        await target.click()
+    }
+
+    async shouldBeVisible(taskName: string){
+        const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
+        await expect(target).toBeVisible()
+    }
+
+    async alertHaveText(text: string){
+        const target = this.page.locator('.swal2-html-container')
+        await expect(target).toHaveText(text)
+    }
+
+    async shouldBeDone(taskName: string){
+        const target = this.page.getByText(taskName)
+        await expect(target).toHaveCSS('text-decoration-line', 'line-through')
+    }
+
+    async shouldNotExist(taskName: string){
+        const target = this.page.locator(`css=.task-item p >> text=${taskName}`)
+        await expect(target).not.toBeVisible()
+    }
+}
